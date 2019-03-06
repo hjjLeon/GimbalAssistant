219e7e8b286cdf57c16c1f71f15f85616cdb7bd6
DESKTOP-M06U3JR\Leon <leon@deltron.tech>
2019-03-06 18:31:32 +0800

    增加波形显示控件

--- GimbleAssistant/Form1.Designer.cs
+++ GimbleAssistant/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
@@ -49,9 +50,12 @@
             this.panel5 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.zGraph1 = new ZvGraph.UI.ZGraph();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -73,11 +77,22 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.textBox2);
             this.panel2.Location = new System.Drawing.Point(168, 236);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(409, 96);
             this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 89);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "发送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
@@ -85,7 +100,7 @@
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(402, 90);
+            this.textBox2.Size = new System.Drawing.Size(350, 90);
             this.textBox2.TabIndex = 0;
             // 
             // panel3
@@ -233,16 +248,71 @@
             // 
             // panel6
             // 
+            this.panel6.Controls.Add(this.button3);
             this.panel6.Location = new System.Drawing.Point(4, 336);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(572, 35);
             this.panel6.TabIndex = 5;
             // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // zGraph1
+            // 
+            this.zGraph1.BackColor = System.Drawing.Color.White;
+            this.zGraph1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.zGraph1.Location = new System.Drawing.Point(580, 7);
+            this.zGraph1.m_backColorH = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.zGraph1.m_backColorL = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYButtonForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_ControlButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_ControlButtonForeColorH = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.zGraph1.m_ControlButtonForeColorL = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.zGraph1.m_ControlItemBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateStringColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateStringTitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_DirectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.zGraph1.m_DirectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_fXBeginSYS = 0F;
+            this.zGraph1.m_fXEndSYS = 60F;
+            this.zGraph1.m_fYBeginSYS = 0F;
+            this.zGraph1.m_fYEndSYS = 1F;
+            this.zGraph1.m_GraphBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_iLineShowColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_iLineShowColorAlpha = 100;
+            this.zGraph1.m_SySnameX = "X轴坐标";
+            this.zGraph1.m_SySnameY = "Y轴坐标";
+            this.zGraph1.m_SyStitle = "波形显示";
+            this.zGraph1.m_titleBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.zGraph1.m_titleColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_titlePosition = 0.4F;
+            this.zGraph1.m_titleSize = 14;
+            this.zGraph1.Margin = new System.Windows.Forms.Padding(0);
+            this.zGraph1.MinimumSize = new System.Drawing.Size(390, 270);
+            this.zGraph1.Name = "zGraph1";
+            this.zGraph1.Size = new System.Drawing.Size(468, 364);
+            this.zGraph1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(434, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 31);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 374);
+            this.ClientSize = new System.Drawing.Size(1054, 376);
+            this.Controls.Add(this.zGraph1);
             this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
@@ -257,6 +327,7 @@
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel6.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -283,6 +354,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private ZvGraph.UI.ZGraph zGraph1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- GimbleAssistant/Form1.cs
+++ GimbleAssistant/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZvGraph.UI;
 
 namespace GimbleAssistant
 {
@@ -17,20 +19,186 @@ namespace GimbleAssistant
             InitializeComponent();
 
             string[] baud = { "43000", "56000", "57600", "115200", "128000", "230400", "256000", "460800" };
-            this.comboBoxBaud.Items.AddRange(baud);
+            comboBoxBaud.Items.AddRange(baud);
 
-            this.comboBoxComNum.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            comboBoxComNum.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
 
-            this.comboBoxBaud.Text = "115200";
-            this.comboBoxDataSize.Text = "8";
-            this.comboBoxjiaoyan.Text = "None";
-            this.comboBoxStopBit.Text = "1";
-            this.button1.Text = "打开串口";
+            comboBoxBaud.Text = "115200";
+            comboBoxDataSize.Text = "8";
+            comboBoxjiaoyan.Text = "None";
+            comboBoxStopBit.Text = "1";
+            button1.Text = "打开串口";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                //将可能产生异常的代码放置在try块中
+                //根据当前串口属性来判断是否打开
+                if (serialPort1.IsOpen)
+                {
+                    //串口已经处于打开状态
+                    serialPort1.Close();    //关闭串口
+                    button1.Text = "打开串口";
+                    button1.BackColor = Color.ForestGreen;
+                    comboBoxBaud.Enabled = true;
+                    comboBoxDataSize.Enabled = true;
+                    comboBoxjiaoyan.Enabled = true;
+                    comboBoxStopBit.Enabled = true;
+                    comboBoxComNum.Enabled = true;
+                    textBox1.Text = "";  //清空接收区
+                    textBox2.Text = "";     //清空发送区
+                }
+                else
+                {
+                    //串口已经处于关闭状态，则设置好串口属性后打开
+                    comboBoxBaud.Enabled = false;
+                    comboBoxDataSize.Enabled = false;
+                    comboBoxjiaoyan.Enabled = false;
+                    comboBoxStopBit.Enabled = false;
+                    comboBoxComNum.Enabled = false;
+                    serialPort1.PortName = comboBoxComNum.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBoxBaud.Text);
+                    serialPort1.DataBits = Convert.ToInt16(comboBoxDataSize.Text);
 
+                    if (comboBoxjiaoyan.Text.Equals("None"))
+                        serialPort1.Parity = System.IO.Ports.Parity.None;
+                    else if (comboBoxjiaoyan.Text.Equals("Odd"))
+                        serialPort1.Parity = System.IO.Ports.Parity.Odd;
+                    else if (comboBoxjiaoyan.Text.Equals("Even"))
+                        serialPort1.Parity = System.IO.Ports.Parity.Even;
+                    else if (comboBoxjiaoyan.Text.Equals("Mark"))
+                        serialPort1.Parity = System.IO.Ports.Parity.Mark;
+                    else if (comboBoxjiaoyan.Text.Equals("Space"))
+                        serialPort1.Parity = System.IO.Ports.Parity.Space;
+
+                    if (comboBoxStopBit.Text.Equals("1"))
+                        serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                    else if (comboBoxStopBit.Text.Equals("1.5"))
+                        serialPort1.StopBits = System.IO.Ports.StopBits.OnePointFive;
+                    else if (comboBoxStopBit.Text.Equals("2"))
+                        serialPort1.StopBits = System.IO.Ports.StopBits.Two;
+
+                    serialPort1.Open();     //打开串口
+                    button1.Text = "关闭串口";
+                    button1.BackColor = Color.Firebrick;
+                }
+            }
+            catch (Exception ex)
+            {
+                //捕获可能发生的异常并进行处理
+
+                //捕获到异常，创建一个新的对象，之前的不可以再用
+                serialPort1 = new System.IO.Ports.SerialPort();
+                //刷新COM口选项
+                comboBoxComNum.Items.Clear();
+                comboBoxComNum.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+                //响铃并显示异常给用户
+                System.Media.SystemSounds.Beep.Play();
+                button1.Text = "打开串口";
+                button1.BackColor = Color.ForestGreen;
+                MessageBox.Show(ex.Message);
+                comboBoxBaud.Enabled = true;
+                comboBoxDataSize.Enabled = true;
+                comboBoxjiaoyan.Enabled = true;
+                comboBoxStopBit.Enabled = true;
+                comboBoxComNum.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                    serialPort1.Write(textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                //捕获可能发生的异常并进行处理
+
+                //捕获到异常，创建一个新的对象，之前的不可以再用
+                serialPort1 = new System.IO.Ports.SerialPort();
+                //刷新COM口选项
+                comboBoxComNum.Items.Clear();
+                comboBoxComNum.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+                //响铃并显示异常给用户
+                System.Media.SystemSounds.Beep.Play();
+                button1.Text = "打开串口";
+                button1.BackColor = Color.ForestGreen;
+                MessageBox.Show(ex.Message);
+                comboBoxBaud.Enabled = true;
+                comboBoxDataSize.Enabled = true;
+                comboBoxjiaoyan.Enabled = true;
+                comboBoxStopBit.Enabled = true;
+                comboBoxComNum.Enabled = true;
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                //因为要访问UI资源，所以需要使用invoke方式同步ui
+                this.Invoke((EventHandler)(delegate
+                {
+                    textBox1.AppendText(serialPort1.ReadExisting());
+                }
+                   )
+                );
+
+            }
+            catch (Exception ex)
+            {
+                //响铃并显示异常给用户
+                System.Media.SystemSounds.Beep.Play();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public List<float> x1 = new List<float>();
+        public List<float> y1 = new List<float>();
+        public List<float> x2 = new List<float>();
+        public List<float> y2 = new List<float>();
+        public List<float> x3 = new List<float>();
+        public List<float> y3 = new List<float>();
+        public List<float> x4 = new List<float>();
+        public List<float> y4 = new List<float>();
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ///-300~num画四条数据
+            button3.Enabled = false;
+            this.Focus();
+            int num;
+            num = 1580;
+            x1.Clear();
+            y1.Clear();
+            x2.Clear();
+            y2.Clear();
+            x3.Clear();
+            y3.Clear();
+            x4.Clear();
+            y4.Clear();
+            for (int i = -300; i < num; i++)
+            {
+                x1.Add(i);
+                y1.Add(i % 1000);
+                x2.Add(i);
+                y2.Add((float)Math.Sin(i / 100f) * 200);
+                x3.Add(i);
+                y3.Add(0);
+                x4.Add(i);
+                y4.Add((float)Math.Sin(i / 100) * 200);
+            }
+            zGraph1.f_ClearAllPix();
+            zGraph1.f_reXY();
+            zGraph1.f_LoadOnePix(ref x1, ref y1, Color.Red, 2);
+            zGraph1.f_AddPix(ref x2, ref y2, Color.Blue, 4);
+            zGraph1.f_AddPix(ref x3, ref y3, Color.FromArgb(0, 128, 192), 2);
+            zGraph1.f_AddPix(ref x4, ref y4, Color.Yellow, 4);
+            zGraph1.f_Refresh();
+            button3.Enabled = true;
         }
     }
 }
