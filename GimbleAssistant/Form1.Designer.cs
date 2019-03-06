5c96ab283d49a7670c5dd7cba6f38392a8c1b21d
DESKTOP-M06U3JR\Leon <leon@deltron.tech>
2019-03-06 15:49:09 +0800

    初步搭建串口助手设置接口

--- GimbleAssistant/Form1.Designer.cs
+++ GimbleAssistant/Form1.Designer.cs
@@ -29,12 +29,260 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxStopBit = new System.Windows.Forms.ComboBox();
+            this.comboBoxjiaoyan = new System.Windows.Forms.ComboBox();
+            this.comboBoxDataSize = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaud = new System.Windows.Forms.ComboBox();
+            this.comboBoxComNum = new System.Windows.Forms.ComboBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(168, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(408, 220);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(402, 212);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Location = new System.Drawing.Point(168, 236);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(409, 96);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(402, 90);
+            this.textBox2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.comboBoxStopBit);
+            this.panel3.Controls.Add(this.comboBoxjiaoyan);
+            this.panel3.Controls.Add(this.comboBoxDataSize);
+            this.panel3.Controls.Add(this.comboBoxBaud);
+            this.panel3.Controls.Add(this.comboBoxComNum);
+            this.panel3.Location = new System.Drawing.Point(4, 7);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(159, 167);
+            this.panel3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "停止位";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "校验位";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "数据位";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "波特率";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "串口号";
+            // 
+            // comboBoxStopBit
+            // 
+            this.comboBoxStopBit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxStopBit.FormattingEnabled = true;
+            this.comboBoxStopBit.Items.AddRange(new object[] {
+            "1",
+            "1.5",
+            "2"});
+            this.comboBoxStopBit.Location = new System.Drawing.Point(63, 107);
+            this.comboBoxStopBit.Name = "comboBoxStopBit";
+            this.comboBoxStopBit.Size = new System.Drawing.Size(93, 20);
+            this.comboBoxStopBit.TabIndex = 4;
+            // 
+            // comboBoxjiaoyan
+            // 
+            this.comboBoxjiaoyan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxjiaoyan.FormattingEnabled = true;
+            this.comboBoxjiaoyan.Items.AddRange(new object[] {
+            "none"});
+            this.comboBoxjiaoyan.Location = new System.Drawing.Point(63, 81);
+            this.comboBoxjiaoyan.Name = "comboBoxjiaoyan";
+            this.comboBoxjiaoyan.Size = new System.Drawing.Size(93, 20);
+            this.comboBoxjiaoyan.TabIndex = 3;
+            // 
+            // comboBoxDataSize
+            // 
+            this.comboBoxDataSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDataSize.FormattingEnabled = true;
+            this.comboBoxDataSize.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBoxDataSize.Location = new System.Drawing.Point(63, 55);
+            this.comboBoxDataSize.Name = "comboBoxDataSize";
+            this.comboBoxDataSize.Size = new System.Drawing.Size(93, 20);
+            this.comboBoxDataSize.TabIndex = 2;
+            // 
+            // comboBoxBaud
+            // 
+            this.comboBoxBaud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBaud.FormattingEnabled = true;
+            this.comboBoxBaud.Location = new System.Drawing.Point(63, 29);
+            this.comboBoxBaud.Name = "comboBoxBaud";
+            this.comboBoxBaud.Size = new System.Drawing.Size(93, 20);
+            this.comboBoxBaud.TabIndex = 1;
+            // 
+            // comboBoxComNum
+            // 
+            this.comboBoxComNum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxComNum.FormattingEnabled = true;
+            this.comboBoxComNum.Location = new System.Drawing.Point(63, 3);
+            this.comboBoxComNum.Name = "comboBoxComNum";
+            this.comboBoxComNum.Size = new System.Drawing.Size(93, 20);
+            this.comboBoxComNum.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(4, 180);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(158, 77);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(5, 263);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(157, 68);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(4, 336);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(572, 35);
+            this.panel6.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(582, 374);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Leon";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxStopBit;
+        private System.Windows.Forms.ComboBox comboBoxjiaoyan;
+        private System.Windows.Forms.ComboBox comboBoxDataSize;
+        private System.Windows.Forms.ComboBox comboBoxBaud;
+        private System.Windows.Forms.ComboBox comboBoxComNum;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- GimbleAssistant/Form1.cs
+++ GimbleAssistant/Form1.cs
@@ -15,6 +15,22 @@ namespace GimbleAssistant
         public Form1()
         {
             InitializeComponent();
+
+            string[] baud = { "43000", "56000", "57600", "115200", "128000", "230400", "256000", "460800" };
+            this.comboBoxBaud.Items.AddRange(baud);
+
+            this.comboBoxComNum.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+
+            this.comboBoxBaud.Text = "115200";
+            this.comboBoxDataSize.Text = "8";
+            this.comboBoxjiaoyan.Text = "None";
+            this.comboBoxStopBit.Text = "1";
+            this.button1.Text = "打开串口";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
